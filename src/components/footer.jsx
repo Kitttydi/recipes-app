3a10e93031bf8af03e190cdd242174902c4894c9
Dmitriy <kate1507@icloud.com>
2024-05-27 19:19:26 +0300

    add files to GitHub

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return <footer className="page-footer  orange accent-3">
+        <div className="footer-copyright">
+            <div className="container">
+            © {new Date().getFullYear()} @Kitttydi
+            <a className="grey-text text-lighten-4 right" href="#!">Repository</a>
+            </div>
+          </div>
+    </footer>
+}
+export {Footer};
